7f96b043a05b0610f5d99205c481e63bc07d81ee
hjb419623273 <396598908@qq.com>
2024-06-29 18:21:41 +0800

    数值保存2

--- Unity/Assets/Scripts/Model/Share/Module/Numeric/INumericWatcher.cs
+++ Unity/Assets/Scripts/Model/Share/Module/Numeric/INumericWatcher.cs
@@ -1,5 +1,12 @@
 ﻿namespace ET
-{
+{    
+	public struct NumbericChange
+	{
+		public Unit Unit;
+		public int NumericType;
+		public long Old;
+		public long New;
+	}
 	public interface INumericWatcher
 	{
 		void Run(Unit unit, NumbericChange args);
